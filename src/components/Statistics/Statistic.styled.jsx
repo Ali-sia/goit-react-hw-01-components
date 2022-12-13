b35639e2499a6146f9bbd542573b6fb16064994e
aliesia-zahorui <aliesia@ukr.net>
2022-12-13 23:04:26 +0200

    красиві стилі для статистики

--- src/components/Statistics/Statistic.styled.jsx
+++ src/components/Statistics/Statistic.styled.jsx
@@ -1,11 +1,21 @@
 import styled from '@emotion/styled';
 
+const randomColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
 export const StatItem = styled.li`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 
+  border: 1px solid gray;
+
+  padding: 10px 0;
+  width: 70px;
+
+  background-color: ${randomColor};
   span:not(:last-child) {
     padding-bottom: 5px;
   }
@@ -20,5 +30,12 @@ export const StatList = styled.ul`
 `;
 
 export const StatSection = styled.div`
+  margin: 25px auto 25px;
+
   text-align: center;
+  background-color: white;
+
+  h2 {
+    text-transform: uppercase;
+  }
 `;
